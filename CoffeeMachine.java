2d071ab7ce4bf97ff8be8b03767accab90c0c59b
buffy <kengallivanu@gmail.com>
2023-05-07 19:29:23 +0300

    Update CoffeeMachine.java

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
         String str = """
@@ -10,5 +12,18 @@ public class CoffeeMachine {
         Coffee is ready!
         """;
         System.out.println(str);
+
+        int water = 200;
+        int milk = 50;
+        int coffee = 15;
+
+        System.out.println("Write how many cups of coffee you will need:");
+        Scanner sc = new Scanner(System.in);
+        int numberOfCups = sc.nextInt();
+
+        System.out.println("For " + numberOfCups + " cups of coffee you will need:");
+        System.out.println(water * numberOfCups + " ml of water");
+        System.out.println(milk * numberOfCups + " ml of milk");
+        System.out.println(coffee * numberOfCups + " g of coffee beans");
     }
 }
