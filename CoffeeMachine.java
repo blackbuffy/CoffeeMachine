b5dab6a3e0a950a1b009ae941867178e485fd2b8
buffy <kengallivanu@gmail.com>
2023-05-07 19:28:38 +0300

    Added CoffeeMachine.java

---
+++ CoffeeMachine.java
@@ -0,0 +1,14 @@
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        String str = """
+        Starting to make a coffee
+        Grinding coffee beans
+        Boiling water
+        Mixing boiled water with crushed coffee beans
+        Pouring coffee into the cup
+        Pouring some milk into the cup
+        Coffee is ready!
+        """;
+        System.out.println(str);
+    }
+}
